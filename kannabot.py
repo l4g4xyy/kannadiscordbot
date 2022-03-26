e8b498fcc4f43bbb5a7b5aa471db3a1ecf6fb44b
Arthur <deboisrodin@gmail.com>
2022-03-26 09:26:08 +0100

    Upgrade

--- kannabot.py
+++ kannabot.py
@@ -20,6 +20,7 @@ bot = commands.Bot(command_prefix="!", description="Kanna Bot")
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game('la version test'))
+    print("Marche !")
 
 
 # Commandes

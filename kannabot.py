b75b6bad9504350d3546a7715aeedc16ec4ec32f
Arthur <deboisrodin@gmail.com>
2022-03-26 20:03:02 +0100

    Upgrade

--- kannabot.py
+++ kannabot.py
@@ -19,7 +19,7 @@ bot = commands.Bot(command_prefix="-")
 @bot.event
 async def on_ready():
     # await bot.change_presence(activity=discord.Game('la version 1.2'))
-    await bot.change_presence(activity=discord.Streaming('son test'))
+    await bot.change_presence(activity=discord.Streaming(name="son test"))
 
 
 # Commandes
